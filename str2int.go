0bc661b0be5256f3e55a0a5dd4ed287cdd848889
artimet123 <maoyongming2512@163.com>
2019-05-20 18:14:41 +0800

    add str 2 int

---
+++ str2int.go
@@ -0,0 +1,54 @@
+package main 
+
+import "fmt"
+
+func main(){
+	var (
+		s = "12344"
+		i int
+	)
+	i = myAtoi(s)
+	fmt.Println("i=", i)
+
+}
+
+func myAtoi(str string) int {
+	if len(str) == 0 {
+		return 0
+	}
+	var (
+		num int
+		neg bool
+	)
+
+	// 去除空格
+	for i, s := range str {
+		if s != ' ' {
+			str = str[i:]
+			break
+		}
+	}
+	if str[0] == '+' {
+		str = str[1:]
+	} else if str[0] == '-' {
+		str = str[1:]
+		neg = true
+	}
+	for _, s := range str {
+		if s < '0' || s > '9' {
+			break
+		}
+		num = num*10 + int(s-'0')
+		// 2147483648 = 2 ^ 31 = 31 << 2
+		if num > 2147483648 {
+			num = 2147483648
+			break
+		}
+	}
+	if neg {
+		num *= -1
+	} else if num > 2147483647 {
+		num = 2147483647
+	}
+	return num
+}
