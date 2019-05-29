4e0ee4e53e790f51caef251f8356240e6b726a4f
artimet123 <maoyongming2512@163.com>
2019-05-29 20:48:30 +0800

    更新幂次方

--- powx-n.go
+++ powx-n.go
@@ -6,11 +6,11 @@ import (
 
 func main() {
 	var (
-		x float64 = 3.2
+		x float64 = 2
 		n int
 	)
 
-	n = -2
+	n = 5
 
 	c := myPow(x, n)
 	fmt.Println("c=", c)
@@ -26,10 +26,12 @@ func myPow(x float64, n int) float64 {
 		ret = 1/ myPow(x, -n)
 	} else if n == 0 {
 		ret = 1
+	} else if n%2 == 0 {
+		ret = myPow(x, n/2)
+		ret *= ret // 正好除2 余0，说明 结果*结果 即可
 	} else {
 		ret = myPow(x, n/2)
-		ret *= ret * x
+		ret *= ret * x // 不能整除时，说明需要加个数字
 	}
 	return ret
-
 }
